3ed2e217bfdc2493ba99f4e35d602293bcdcbccc
HsuLeii <47671436+HsuLeii@users.noreply.github.com>
2022-02-25 11:55:31 +0800

    0225

--- js/all.js
+++ js/all.js
@@ -20,35 +20,147 @@ $(".language_dropdown").click(function () {
   $(".about_dropdown").children(".dropdown_content").removeClass("open");
 });
 // 彈跳視窗資訊書籍選擇開始
-$('.modal_btn ul li.introduction').click(function(){
+$('.modal_btn ul li.introduction').click(function () {
   $('.modal_btn ul li').removeClass('active');
   $('.modal_btn ul li.introduction').addClass('active');
   var selector = $('.modal_btn ul li.introduction').attr('data_filter');
-    $('.introduction_item').isotope({
-      filter: selector
-    })
+  $('.introduction_item').isotope({
+    filter: selector
+  })
 });
 $('.item.vocabulary').hide();
-$('.modal_btn ul li.vocabulary').click(function(){
+$('.modal_btn ul li.vocabulary').click(function () {
   $('.modal_btn ul li').removeClass('active');
   $('.modal_btn ul li.vocabulary').addClass('active');
   $('.item.vocabulary').addClass('active');
   $('.item.vocabulary').show();
   var selector = $('.modal_btn ul li.vocabulary').attr('data_filter');
-    $('.introduction_item').isotope({
-      filter: selector
-    })
+  $('.introduction_item').isotope({
+    filter: selector
+  })
 });
 // 彈跳視窗資訊書籍選擇結束
 // 常見問題開始
-
-    $("div.faq_answer").hide(); //子選單(div.faq-answer)隱藏
-  $(".faq_question").click(function () {
-    $("div.faq_answer").slideUp();
-    if ($("+div", this).css("display") == "none") {
-      $("+div", this).slideDown();
-      $(".faq_question").children("span").removeClass("up");
-      $(this).children("span").toggleClass("up");
-    } 
-  })
+$("div.faq_answer").hide();
+$(".faq_question").click(function () {
+  $("div.faq_answer").slideUp();
+  if ($("+div", this).css("display") == "none") {
+    $("+div", this).slideDown();
+    $(".faq_question").children("span").removeClass("up");
+    $(this).children("span").toggleClass("up");
+  }
+})
 // 常見問題結束
+// 分級對應開始
+$(".grade_content").hide();
+$(".first_grade_content").show();
+$(".first_grade_background").children("p").addClass("active");
+$(".first_grade_background").click(function () {
+  $(".grade_content").hide();
+  $(".first_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+})
+.hover(function() {
+  $(".grade_content").hide();
+  $(".first_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+});
+$(".second_grade_background").click(function () {
+  $(".grade_content").hide();
+  $(".second_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+})
+.hover(function() {
+  $(".grade_content").hide();
+  $(".second_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+});
+$(".third_grade_background").click(function () {
+  $(".grade_content").hide();
+  $(".third_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+})
+.hover(function() {
+  $(".grade_content").hide();
+  $(".third_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+});
+$(".fourth_grade_background").click(function () {
+  $(".grade_content").hide();
+  $(".fourth_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+})
+.hover(function() {
+  $(".grade_content").hide();
+  $(".fourth_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+});
+$(".fifth_grade_background").click(function () {
+  $(".grade_content").hide();
+  $(".fifth_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+})
+.hover(function() {
+  $(".grade_content").hide();
+  $(".fifth_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+});
+$(".sixth_grade_background").click(function () {
+  $(".grade_content").hide();
+  $(".sixth_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+})
+.hover(function() {
+  $(".grade_content").hide();
+  $(".sixth_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+});
+$(".seventh_grade_background").click(function () {
+  $(".grade_content").hide();
+  $(".seventh_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+})
+.hover(function() {
+  $(".grade_content").hide();
+  $(".seventh_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+});
+$(".eighth_grade_background").click(function () {
+  $(".grade_content").hide();
+  $(".eighth_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+})
+.hover(function() {
+  $(".grade_content").hide();
+  $(".eighth_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+});
+$(".ninth_grade_background").click(function () {
+  $(".grade_content").hide();
+  $(".ninth_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+})
+.hover(function() {
+  $(".grade_content").hide();
+  $(".ninth_grade_content").show();
+  $(".bar").children("p").removeClass("active");
+  $(this).children("p").addClass("active");
+});
+// 分級對應結束
